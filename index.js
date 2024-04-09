28076f7834e0f74a2a9a34a705828d21398c672f
Rethabile Diale <rethadiale27@gmail.com>
2024-04-09 19:58:38 +0200

    commit

--- index.js
+++ index.js
@@ -40,19 +40,3 @@ console.log(branchB.getBranchInfo());
 
 //    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
 console.log(branchA === branchB);
-
-// This pseudo-code provides a step-by-step explanation of how to implement the Singleton Pattern for managing a single instance of the `BankBranch` class throughout the application, ensuring that multiple instances refer to the same object.
-
-// NOTES:
-// This example demonstrates the Singleton pattern by creating a single instance of the `BankBranch` class.
-// Even if we attempt to create another instance of the branch, we receive the original instance,
-// ensuring that there is only one set of branch information throughout the application.
-
-// - We define a `BankBranch` class that represents bank branch information.
-// - We use the Singleton pattern to ensure that there is only one instance of the `BankBranch` class.
-// - The `bankBranchInstance` variable is used to hold the single instance of the class.
-// - When a new `BankBranch` instance is created, it checks if `bankBranchInstance` already exists.
-//   If it does, it returns the existing instance; otherwise, it creates a new one.
-// - The `getBranchInfo` method allows us to retrieve the branch information from the singleton instance.
-// - We demonstrate the Singleton pattern by creating two instances (`branchA` and `branchB`) and show that they both point to the same instance,
-//   ensuring that there is only one set of branch information throughout the application.
